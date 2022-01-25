4fc4699c6c5b60c4f8426da203344d1d181644bf
SirBurton <93293241+aburtonProto@users.noreply.github.com>
2022-01-25 13:54:53 -0700

    make the version higher

--- continuousprint/__init__.py
+++ continuousprint/__init__.py
@@ -355,8 +355,3 @@ def __plugin_load__():
 	global __plugin_implementation__
 	__plugin_implementation__ = ContinuousprintPlugin()
 
-	#global __plugin_hooks__
-	#__plugin_hooks__ = {
-	#	"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-	#}
-
